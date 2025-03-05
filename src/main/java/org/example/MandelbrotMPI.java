f892483fb793807d9de4257d890e0352779cd8de
Tea Pashovska <pasovskatea@gmail.com>
2025-03-05 15:43:13 +0100

    Distributing rows

--- src/main/java/org/example/MandelbrotMPI.java
+++ src/main/java/org/example/MandelbrotMPI.java
@@ -9,8 +9,14 @@ public class MandelbrotMPI {
         int rank = MPI.COMM_WORLD.Rank();
         int size = MPI.COMM_WORLD.Size();
 
-        System.out.println("Hello from process " + rank + " out of " + size);
-        
+        int rowsPerProcess = 100;
+        int startRow = rowsPerProcess * rank;
+        int endRow = startRow + rowsPerProcess;
+
+        System.out.println("Process " + rank + " started...");
+        System.out.println("Process " + rank + " got assigned rows " + startRow + " to " + endRow);
+        System.out.println("Process " + rank + " waiting for next step...");
+
         MPI.Finalize();
     }
 }
